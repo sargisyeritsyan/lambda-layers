e5b58aa3f139eb51b6798347e23cabbf066dbe7f
sargisyeritsyan <sargisy@doublecoconut.com>
2022-08-11 18:19:01 +0400

    first commit

---
+++ handler.js
@@ -0,0 +1,12 @@
+const moment = require("moment");
+const dateNow = moment().format("MMM Do YY");
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: dateNow
+      }),
+  };
+};
